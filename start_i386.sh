a8592723216a889ef85897a7f13b4ef53c22f920
f4rnham <f4rnham@users.noreply.github.com>
2018-02-18 22:54:52 +0100

    Custom configuration

--- start_i386.sh
+++ start_i386.sh
@@ -21,5 +21,5 @@ else
     snapshot="-loadvm $2"
 fi
 
-cp pyrebox.conf.WinXPSP3x86 pyrebox.conf
-./pyrebox-i386 -monitor stdio -m 256 -usb -usbdevice tablet -drive file=$1,index=0,media=disk,format=qcow2,cache=unsafe -vnc 127.0.0.1:0 ${snapshot}
+cp pyrebox.conf.Win7SP1x86 pyrebox.conf
+./pyrebox-i386 -s -monitor stdio -m 1024 -usb -usbdevice tablet -drive file=$1,index=0,media=disk,format=qcow2,cache=unsafe -netdev tap,id=t0,ifname=tap0,script=no,downscript=no -device e1000,netdev=t0,id=nic0 -device rtl8139,netdev=n0 -netdev user,id=n0,smb=/mnt/hgfs/scripts/,smbserver=10.0.2.4 -vnc 127.0.0.1:0 ${snapshot}
